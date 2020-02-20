15fb99dea75fd70bce8be4730be795c197ff6c00
adumitrescu1106 <maxandyx13@gmail.com>
2020-02-20 21:55:05 +0200

    input

--- src/main/Input.java
+++ src/main/Input.java
@@ -1,15 +1,33 @@
 package main;
 
+import Library.Library;
+
+import java.util.HashMap;
 import java.util.List;
 
 public class Input {
     // ce ne trebuie
+    private final HashMap<Integer, Integer> sortedMap;
+    private final List<Library> libraries;
 
+    public Input(final HashMap<Integer, Integer> sortedMap, final List<Library> libraries) {
+        this.sortedMap = sortedMap;
+        this.libraries = libraries;
+    }
     public Input() {
+        this.sortedMap = null;
+        this.libraries = null;
     }
 
-
     //Getteri
+
+    public HashMap<Integer, Integer> getSortedMap() {
+        return sortedMap;
+    }
+
+    public List<Library> getLibraries() {
+        return libraries;
+    }
 
     public final boolean isValidInput() {
         return false;
