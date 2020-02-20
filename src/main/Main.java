af5d680a756125c666aaa19582525c4c9941b6b0
AdiDumi <adrian.dumitrescu99@stud.acs.upb.ro>
2020-02-20 22:21:34 +0200

    constr

--- src/main/Main.java
+++ src/main/Main.java
@@ -27,24 +27,26 @@ public final class Main {
         }
 
 
-        int days, minDays, scanDays;
-        List<Integer> answerBooks = new ArrayList<>();
-        List<Integer> answerLib = new ArrayList<>();
-        List<Integer> bookPerLib = new ArrayList<>();
         List<Library> allLibraries = new ArrayList<Library>();
         HashMap<Integer, Integer> sortedMap = new HashMap<>();
 
+        int days, minDays, scanDays, nrBooks;
+        List<Integer> answerBooks = new ArrayList<>();
+        List<Integer> answerLib = new ArrayList<>();
+        List<Integer> bookPerLib = new ArrayList<>();
         // get the input
         days = gameInput.getNoDays();
         allLibraries = gameInput.getLibraries();
         sortedMap = gameInput.getSortedMap();
 
+        Library minLib;
         while (days <= 0) {
             minDays = days;
+            minLib = null;
             for (Library currLib : allLibraries) {
                 if (currLib.getSignDays() < minDays) {
                     minDays = currLib.getSignDays();
-                    break;
+                    minLib = currLib;
                 }
             }
             scanDays = minDays;
@@ -52,15 +54,27 @@ public final class Main {
                 scanDays = days;
             }
             days -= minDays;
-            answerLib.add(currLib.getId());
-            allLibraries.remove(currLib);
+            answerLib.add(minLib.getId());
+            allLibraries.remove(minLib);
+            nrBooks = 0;
             while (scanDays != 0) {
-                for (int i = 0; i < currLib.getBookPerDay()) {
-                    answerBooks = currLib.getBooks().get(i);
-                    currLib.updateBooks();
+                for (int i = 0; i < minLib.getBookPerDay(); ++i) {
+                    answerBooks.add(minLib.getBooks().get(i));
+                    minLib.updateBooks(sortedMap.get(minLib.getBooks().get(i)), minLib.getBooks().get(i));
+                    for (Library restLib : allLibraries) {
+                        if (restLib.getId() != minLib.getId()) {
+                            for (Integer sameBook : restLib.getBooks()) {
+                                if (sameBook == minLib.getBooks().get(i)) {
+                                    restLib.updateBooks(sortedMap.get(minLib.getBooks().get(i)), minLib.getBooks().get(i));
+                                }
+                            }
+                        }
+                    }
                 }
                 scanDays--;
+                nrBooks++;
             }
+            bookPerLib.add(nrBooks);
         }
     }
 }
