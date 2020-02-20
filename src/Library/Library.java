3f01b48d5a343d0347838c6d1c4cc8aaec99957b
AdiDumi <adrian.dumitrescu99@stud.acs.upb.ro>
2020-02-20 21:30:54 +0200

    Library all

--- src/Library/Library.java
+++ src/Library/Library.java
@@ -4,19 +4,22 @@ package Library;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Library {
-    private List<Integer> books = new ArrayList<>();
+    private List<Integer> books;
+    private int id;
     private int signDays;
     private int bookPerDay;
     private int maxPoints;
+    private int nrBooks;
 
-    Library(final int signDays, final int bookPerDay, final ArrayList<Integer> books) {
+    Library(final int signDays, final int bookPerDay, final ArrayList<Integer> books, final int id) {
+        this.id = id;
         this.signDays = signDays;
         this.bookPerDay = bookPerDay;
         this.books = books;
         this.maxPoints = 0;
+        this.nrBooks = books.size();
     }
 
     public List<Integer> getBooks() {
@@ -47,6 +50,14 @@ public class Library {
         this.maxPoints -= point;
     }
 
+    public int getNrBooks() {
+        return nrBooks;
+    }
+
+    public void setNrBooks(int nrBooks) {
+        this.nrBooks = nrBooks;
+    }
+
     public void calcMaxPoints(final HashMap<Integer, Integer> books) {
          for (Integer value : books.values()) {
              this.maxPoints += value;
@@ -57,4 +68,29 @@ public class Library {
         return this.maxPoints;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    List<Integer> answerBooks = new ArrayList<>();
+    List<Integer> answerLib = new ArrayList<>();
+    List<Integer> bookPerLib = new ArrayList<>();
+    while (days <= 0) {
+        minDays = days;
+        for (Library currLib : allLibraries) {
+            if (currLib.getSignDays() < minDays) {
+                minDays = currLib.getSignDays();
+            }
+        }
+        scanDays = minDays;
+        days -= minDays;
+        answerLib.add(currLib.getId());
+        while (scanDays != 0) {
+            for (int i = 0; i < )
+        }
+    }
 }
